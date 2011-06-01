d30a1b29631801e1a37a50db42d5b71bd432cc4e
Thomas Clement Mogensen <mogensen@diku.dk>
2011-06-01 21:55:05 +0200

    added vowel counting

--- stats.rb
+++ stats.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require 'rubygems'
+require 'jcode'
+
+$KCODE = 'UTF-8'
 
 data = File.read('stopwords.txt')
 words = data.split(',')
 puts "Number of words: " + words.size.to_s
 puts "Average word langth: " + data.size.fdiv(words.size).to_s
+puts "Number of vowels: " + data.jcount("aeiouæøå").to_s
+
